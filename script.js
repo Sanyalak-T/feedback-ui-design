555c3f6844bbbedbdcd183e07e831e02958ce886
Sanyalak-T <isanyalakt@gmail.com>
2021-11-27 00:30:41 +0700

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-const codes = document.querySelectorAll('.code')
+const ratings = document.querySelectorAll('.rating')
+const ratingsContainer = document.querySelector('.ratings-container')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+let selectedRating = 'Satisfied'
 
-codes[0].focus()
-
-codes.forEach((code, idx) => {
-    code.addEventListener('keydown', (e) => {
-        codes[idx].value = ''
-        if(e.key >= 0 && e.key <= 9) {
-            setTimeout(() => codes[idx + 1].focus(), 10)
-        }else if(e.key === 'Backspace') {
-            setTimeout(() => codes[idx - 1].focus(), 10)
-        }
-    })
+ratingsContainer.addEventListener('click', (e) => {
+    if(e.target.parentNode.classList.contains('rating')) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
+    }
 })
+
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = `
+    <i class="fas fa-heart"></i>
+    <strong>Thank You!</strong>
+    <br>
+    <strong>Feedback: ${selectedRating}</strong>
+    <p>We'll use your feedback to improve our customer support</p>
+    `
+})
+
+
+function removeActive() {
+    for(let i = 0; i < ratings.length; i++) {
+        ratings[i].classList.remove('active')
+    }
+}
